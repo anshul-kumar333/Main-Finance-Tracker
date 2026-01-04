774126ddfc889cfc7e185d2a03c8082c6757af16
anshul-kumar333 <opo160591@gmail.com>
2026-01-04 14:45:26 +0530

    Update server.js

--- server.js
+++ server.js
@@ -8,7 +8,10 @@ connectDB();
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+  origin: "*"
+}));
+
 app.use(express.json());
 
 app.use("/api/auth", require("./routes/authRoutes"));
@@ -22,3 +25,4 @@ const PORT = process.env.PORT || 5000;
 app.listen(PORT, () =>
     console.log(`🚀 Server running on port ${PORT}`)
 );
+
