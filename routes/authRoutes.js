ec038dce1fc9e53324ce2fb3f1194a5aeeb445a1
anshul-kumar333 <opo160591@gmail.com>
2026-01-05 01:11:09 +0530

    Add files via upload

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -58,7 +58,15 @@ router.post("/login", async (req, res) => {
             { expiresIn: "7d" }
         );
 
-        res.json({ token });
+        res.json({
+            token,
+            user: {
+                id: user._id,
+                name: user.name,
+                email: user.email
+            }
+        });
+
     } catch (err) {
         res.status(500).json({ message: "Server error" });
     }
--- server.js
+++ server.js
@@ -8,10 +8,7 @@ connectDB();
 
 const app = express();
 
-app.use(cors({
-  origin: "*"
-}));
-
+app.use(cors());
 app.use(express.json());
 
 app.use("/api/auth", require("./routes/authRoutes"));
@@ -25,4 +22,3 @@ const PORT = process.env.PORT || 5000;
 app.listen(PORT, () =>
     console.log(`🚀 Server running on port ${PORT}`)
 );
-
